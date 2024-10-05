657427250960c70571901d107a5493b000c28575
Emre Kapukaya <kapukayaemre@hotmail.com>
2024-10-05 12:42:13 +0300

    speaker form located in model, conference resource layout updated

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -3,6 +3,17 @@
 namespace App\Models;
 
 use App\Enums\Region;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Get;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -19,6 +30,93 @@ class Conference extends Model
         'region' => Region::class,
         'venue_id' => 'integer',
     ];
+
+    public static function getForm(): array
+    {
+        return [
+            Tabs::make()
+                ->columnSpanFull()
+                ->tabs([
+                    Tabs\Tab::make('Conference Details')
+                        ->schema([
+                            Section::make('Conference Details')
+                                // ->aside() description left and form right side
+                                ->description('Some description about conference section')
+                                ->icon('heroicon-o-information-circle')
+                                ->columns(2) // ['md' => 2, 'lg' => 3] responsive layout
+                                ->collapsible()
+                                ->schema([
+                                    TextInput::make('name')
+                                        ->columnSpanFull()
+                                        ->label('Conference Name')
+                                        ->default('My Conference')
+                                        ->helperText('The name of the conference.')
+                                        ->required()
+                                        ->maxLength(60),
+                                    RichEditor::make('description')
+                                        ->columnSpanFull()
+                                        ->disableToolbarButtons(['italic'])
+                                        ->required(),
+                                    DateTimePicker::make('start_date')
+                                        ->displayFormat('d.m.Y')
+                                        ->native(false)
+                                        ->required(),
+                                    DateTimePicker::make('end_date')
+                                        ->native(false)
+                                        ->required(),
+                                    Fieldset::make('Status')
+                                        ->columns(2)
+                                        ->schema([
+                                            Select::make('status')
+                                                ->placeholder('Select Status')
+                                                ->label('')
+                                                ->options([
+                                                    'draft' => 'Draft',
+                                                    'published' => 'Published',
+                                                    'archived' => 'Archived'
+                                                ])
+                                                ->required(),
+                                            Toggle::make('is_published')
+                                                ->default(true)
+                                        ])
+                                ])
+                        ]),
+                    Tabs\Tab::make('Location')
+                        ->schema([
+                            Section::make('Location')
+                                ->collapsible()
+                                ->columns(2)
+                                ->schema([
+                                    Select::make('region')
+                                        ->live()
+                                        ->enum(Region::class)
+                                        ->options(Region::class)
+                                        ->required(),
+                                    Select::make('venue_id')
+                                        ->searchable()
+                                        ->preload()
+                                        ->createOptionForm(Venue::getForm())
+                                        ->editOptionForm(Venue::getForm())
+                                        ->relationship('venue', 'name', modifyQueryUsing: function (Builder $query, Get $get) {
+                                            return $query->where('region', $get('region'));
+                                        }),
+                                ])
+                        ]),
+                    Tabs\Tab::make('Speakers')
+                        ->schema([
+                            CheckboxList::make('speakers')
+                                ->searchable()
+                                ->bulkToggleable()
+                                ->columns(3)
+                                ->relationship('speakers', 'name')
+                                ->options(
+                                    Speaker::all()->pluck('name', 'id')
+                                )
+                                ->required()
+                        ])
+                ]),
+        ];
+    }
 
     public function venue(): BelongsTo
     {
--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -11,8 +14,56 @@ class Speaker extends Model
     use HasFactory;
 
     protected $casts = [
-        'id' => 'integer',
+        'id'             => 'integer',
+        'qualifications' => 'array'
     ];
+
+    public static function getForm(): array
+    {
+        return [
+            TextInput::make('name')
+                ->required(),
+            TextInput::make('email')
+                ->email()
+                ->required(),
+            RichEditor::make('bio')
+                ->required()
+                ->columnSpanFull(),
+            TextInput::make('twitter_handle')
+                ->required(),
+            CheckboxList::make('qualifications')
+                ->columnSpanFull()
+                ->searchable()
+                ->bulkToggleable() // Select all
+                ->options([
+                        'business-leader'       => 'Business Leader',
+                        'charisma'              => 'Charismatic Speaker',
+                        'first-time'            => 'First Time Speaker',
+                        'hometown-hero'         => 'Hometown Hero',
+                        'humanitarian'          => 'Works in Humanitarian Field',
+                        'laracasts-contributor' => 'Laracasts Contributor',
+                        'twitter-influencer'    => 'Large Twitter Following',
+                        'youtube-influencer'    => 'Large Youtube Following',
+                        'open-source'           => 'Open Source Creator / Maintainer',
+                        'unique-perspective'    => 'Unique Perspective',
+                    ]
+                )
+                ->descriptions([
+                        'business-leader'       => 'Business Leader Description',
+                        'charisma'              => 'Charismatic Speaker Description',
+                        'first-time'            => 'First Time Speaker Description',
+                        'hometown-hero'         => 'Hometown Hero Description',
+                        'humanitarian'          => 'Works in Humanitarian Field Description',
+                        'laracasts-contributor' => 'Laracasts Contributor Description',
+                        'twitter-influencer'    => 'Large Twitter Following Description',
+                        'youtube-influencer'    => 'Large Youtube Following Description',
+                        'open-source'           => 'Open Source Creator / Maintainer Description',
+                        'unique-perspective'    => 'Unique Perspective Description',
+                    ]
+                )
+                ->columns(3)
+        ];
+    }
 
     public function conferences(): BelongsToMany
     {
